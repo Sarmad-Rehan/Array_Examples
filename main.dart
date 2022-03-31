9e044b27918793418644656d83fd5557aff9f398
Sarmad Rehan <sarmadrehan52@gmail.com>
2022-03-31 10:10:27 +0500

    Assignment

---
+++ main.dart
@@ -0,0 +1,30 @@
+// Program 7.5
+
+import 'dart:io';
+
+void main(List<String> args) {
+  int count = 0;
+  final noOfPersons = [];
+
+  // daysInMonths.setAll(0, [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]);
+  // stdout.write("Enter the Month Number: ");
+  // String? inputMonthNumber = stdin.readLineSync();
+  // int? monthNumber = int.tryParse(inputMonthNumber!);
+
+  stdout.write("Enter number of persons: ");
+  String? inputNumPersons = stdin.readLineSync();
+  int? numPerson = int.tryParse(inputNumPersons!);
+
+  for (int i = 0; i < numPerson! - 1; i++) {
+    stdout.write("Enter Age: ");
+    String? inputAge = stdin.readLineSync();
+    int? age = int.tryParse(inputAge!);
+    noOfPersons[i] = age;
+    if (noOfPersons[i] >= 50 && noOfPersons[i] <= 60) {
+      count++;
+    } else {
+      noOfPersons[i] = age;
+    }
+  }
+  print("The number of Persons between 50 and 60 are: $count");
+}
